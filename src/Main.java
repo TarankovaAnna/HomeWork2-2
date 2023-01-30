fc9cc82af2bb526bb5f48f776eeab5eab73a714b
Anna Tarankova <tarankovaanna@gmail.com>
2023-01-30 19:36:48 +0300

    Add src/

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 10;
+        int deposit = 1100;
+
+        int finalAccount;
+        int bonus;
+
+        if (deposit>1000) {
+            bonus = deposit/100;
+            finalAccount = account + deposit;
+        } else {
+            bonus = 0;
+            finalAccount = account + deposit;
+        }
+        System.out.println("Итоговый счет:" + finalAccount);
+        System.out.println("Начислено бонусных рублей:" + bonus);
+    }
+}
